ba47a1e4591defca777736dc7fa359b34e3f1811
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2022-05-17 18:18:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisibility_rule_of_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int rem;
+            int sum = 0;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 11 == 0)
+            {
+                Console.WriteLine("{0} is divisible by 11", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not divisible by 11", n);
+            }
+            Console.ReadLine();
+            
+                
+
+
+
+               
+        }
+    }
+}
