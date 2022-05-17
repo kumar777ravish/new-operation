8402c68be8d98d0738abfe39bb7df4cdb7324795
RAVISH KUMAR <89187239+kumar777ravish@users.noreply.github.com>
2022-05-17 18:20:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,33 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace divisibility_rule_of_11
+namespace divisible_9
 {
     class Program
     {
         static void Main(string[] args)
         {
             int n;
-            int rem;
             int sum = 0;
+            int rem = 0;
+            int res = 0;
             Console.WriteLine("Enter a number");
             n = Convert.ToInt32(Console.ReadLine());
-
-            if (n % 11 == 0)
+            res = n;
+            while (res > 0)
             {
-                Console.WriteLine("{0} is divisible by 11", n);
+                rem = res % 10;
+                sum = sum + rem;
+                res = res / 10;
+            }
+            Console.WriteLine(sum);
+            if (n % 3 == 0)
+            {
+                Console.WriteLine("{0} is divisible by 9", n);
             }
             else
             {
-                Console.WriteLine("{0} is not divisible by 11", n);
+                Console.WriteLine("{0} is not divisible by 9", n);
             }
+
             Console.ReadLine();
-            
-                
-
-
-
-               
         }
     }
 }
+
+
